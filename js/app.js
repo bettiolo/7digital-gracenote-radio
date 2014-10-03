0bcdde4f162fa4a481ff20e01089afe8aebaac1b
Marco Bettiolo <marco@bettiolo.it>
2014-10-03 11:59:12 +0100

    adding browser's local storage support

--- js/app.js
+++ js/app.js
@@ -1,7 +1,16 @@
-var app = angular.module('7gRadio', []);
+var app = angular.module('7gRadio', ['ngStorage']);
 
-app.controller('radioController', function ($scope) {
+app.controller('radioController', function ($scope, $localStorage) {
 	'use strict';
-	$scope.gracenote = {};
-	$scope.gracenote.clientId = 'test';
+	var defaultSettings = {
+		gracenoteClientId: 'Your Gracenote Client ID',
+		sdConsumerKey: 'Your 7digital Consumer Key',
+		sdConsumerSecret: 'Your 7digital Consumer Secret'
+	};
+	$scope.$storage = $localStorage.$default(defaultSettings);
+	$scope.reset = function () {
+		$scope.$storage.$reset(defaultSettings);
+	};
 });
+
+// https://c{{clientId}}.web.cddbp.net/webapi/
