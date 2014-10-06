a5885bebcfa785314f04ba157f3dfcc176216b73
Marco Bettiolo <marco@bettiolo.it>
2014-10-06 18:49:57 +0100

    Work in progress integrating with Gracenote

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var app = angular.module('7gRadio', ['ngStorage']);
 
-app.controller('radioController', function ($scope, $localStorage) {
+app.controller('radioController', function ($scope, $localStorage, $window) {
 	'use strict';
 	var defaultSettings = {
 		gracenoteClientId: 'Your Gracenote Client ID',
@@ -9,8 +9,12 @@ app.controller('radioController', function ($scope, $localStorage) {
 	};
 	$scope.$storage = $localStorage.$default(defaultSettings);
 	$scope.reset = function () {
-		$scope.$storage.$reset(defaultSettings);
+		if ($window.confirm('Are you sure you want to reset your settings?')) {
+			$scope.$storage.$reset(defaultSettings);
+		}
+	};
+	$scope.test = function () {
+		var rythmApi = new gracenote.RythmApi($scope.$storage.gracenoteClientId);
+		rythmApi.register('marco.bettiolo@7digital.com');
 	};
 });
-
-// https://c{{clientId}}.web.cddbp.net/webapi/
