c009ca0e0c186e4835d8233e1053ce65023108db
Marco Bettiolo <marco@bettiolo.it>
2014-10-09 02:35:18 +0100

    Moved out of the way socket code

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,21 +1,19 @@
 angular.module('7gRadio.controllers', [])
 	.controller('mainController', function ($scope, $localStorage, $window, socket, radioApi) {
 		'use strict';
+		socket.on('client:connected', function (data) {
+			console.log('Socket.IO connected. Client ID: ' + data.id);
+		});
 		var defaultSettings = {
 			email: 'Your Email',
 			gracenoteUserId: ''
 		};
 		$scope.$storage = $localStorage.$default(defaultSettings);
-		console.log($scope.$storage);
 		$scope.reset = function () {
 			if ($window.confirm('Are you sure you want to reset your settings?')) {
 				$scope.$storage.$reset(defaultSettings);
 			}
 		};
-		socket.on('client:connected', function (data) {
-			console.log('Client ID: ' + data.id);
-		});
-
 		$scope.createUser = function () {
 			radioApi.createUser.get({ email : $scope.$storage.email })
 				.$promise
