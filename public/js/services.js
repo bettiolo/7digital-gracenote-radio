c23584cdc1a6f15c9aef8d3088d24dd5dd754a7a
Marco Bettiolo <marco@bettiolo.it>
2014-10-09 02:29:03 +0100

    Added radio API stub angular service

--- public/js/services.js
+++ public/js/services.js
@@ -1,4 +1,9 @@
 angular.module('7gRadio.services', [])
 	.factory('socket', function (socketFactory) {
 		return socketFactory();
+	})
+	.factory('radioApi', function($resource){
+		return {
+			createUser : $resource('/api/user/create/:email')
+		};
 	});
